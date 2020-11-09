4fb21c2a872e3b19d926627208a2d7becc39ad08
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2020-11-09 11:45:28 +0000

    added js for search facility in 'Why FAC?' section, moved timer js to 'robot.js'; Debugging still outstanding on 'Why FAC' search facility.

--- whyFAC.js
+++ whyFAC.js
@@ -1,39 +1,53 @@
 // ******** WHY FAC? SECTION OF WEBSITE ********
 
 
-// Initialse variable with the date of next Founders and Coders course
-const courseDate = new Date("Mar 3, 2021 09:45:00").getTime();
+// Initialise js variables for search and suggestion elements
+const searchBox2 = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
 
-let days;
-let hours;
-let minutes;
-let seconds;
+// Initialise variable for endpoint location of reference json file stored as a gist
+const endpoint = 'https://gist.githubusercontent.com/ByteSizedIT/1c8561bb56e71e56a93d77a9a40741d9/raw/264477a4f1c159bbc0bc01db39b376db2a16168a/FACglossary.json';
 
-const countDown = setInterval(() => {
+// Initialise variable/array for storing entries returned frin reference gist file
+const entries = [];
 
-  // Initialise variable with today's date and time
-  const now = new Date().getTime();
 
-  // Initialise variable with timeframe from now until count down date
-  const timeframe = courseDate - now;
+// Use fetch API method, passing in the endpoint of the json reference file
+fetch(endpoint)
+    //convert/identify raw data returned in to a json format to make it readable
+    .then(response => response.json())
+    //spread each individual item from the json file on to the end of the entries array
+    .then(data => entries.push(...data));
 
-  //  Calculate days, hours, minutes and seconds
-  days = Math.floor(timeframe / (1000 * 60 * 60 * 24));
-  hours = Math.floor((timeframe % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  minutes = Math.floor((timeframe % (1000 * 60 * 60)) / (1000 * 60));
-  seconds = Math.floor((timeframe % (1000 * 60)) / 1000);
 
-  // If timeframe is positive numer
-  if (timeframe > 0) {
-    // Output days, hours, mins, secs to course in html element with id="cdTimer"
-    document.getElementById("preTimer").innerHTML = "Countdown to Founders & Coders Spring 2021: ";
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    document.getElementById("postTimer").style.display = "none";
-  }
-  else {
-    // Output days, hours, mins, secs to course in html element with id="cdTimer"
-    document.getElementById("preTimer").innerHTML = "Time since Founders & Coders Spring 2021 started: ";
-    document.getElementById("timer").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-    document.getElementById("postTimer").style.display = "block";
-  }
-}, 1000);
+// Create a function to filter entries returned by fetch API
+function findMatches(searchText, entries) {
+  return entries.filter(entry => {
+      const searchReg = new RegExp(searchText, 'gi');
+      return entry.term.match(searchReg) || entry.definition.match(searchReg);
+  })
+}
+
+// Create a function to display found matches
+function displayMatches(e) {
+  //console.log(e);
+  //console.log(this);
+  //console.dir(this);
+  const matches = findMatches(this.value, entries);
+  const searchReg = new RegExp(this.value, 'gi');
+  const html = matches.map(entry => {
+      const matchedTerm = entry.term.replace(searchReg, `<span class='highlight'>${this.value}</span>`);
+      const matchedDef = entry.definition.replace(searchReg, `<span class='highlight'>${this.value}</span>`);
+      const matchedLinks = entry.link.split(" ").map(link => {return `</br><a href="${link}" target="_blank">${link}</a>`}).join("");
+      return `
+      <li>
+          <!--<span><b>${entry.term}:</b> ${entry.definition}</span>-->
+          <span><b>${matchedTerm}:</b> ${matchedDef}</span></br>
+          <span><b>links:</b> ${matchedLinks}</span>
+      </li>
+      `;
+  }).join("");
+  suggestions.innerHTML = html;
+}
+  
+  searchBox2.addEventListener('keyup', displayMatches);
