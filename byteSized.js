256a65d8ab3962948d16b6d8dbab844ea7800bc0
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2020-11-09 19:35:15 +0000

    added html, css and js for links to byteSizedIT projects.

--- byteSized.js
+++ byteSized.js
@@ -1,3 +1,16 @@
 // ******** BYTESIZEDIT SECTION OF WEBSITE ********
 
 
+const reviews = document.getElementsByClassName("review");
+
+for (let i = 0; i < reviews.length; i++) {
+  reviews[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    };
+  });
+}
