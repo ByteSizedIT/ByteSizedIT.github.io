56085a8b57dbd50cd23e6120ba1e71498a346b36
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2020-12-19 21:54:11 +0000

    Added portrait slides for carousel, along with css and js.

--- home.js
+++ home.js
@@ -25,14 +25,19 @@ window.addEventListener('load', padSection);
 // ******** CAROUSEL SECTION OF WEBSITE ********
 
 
-//Initialise carousel-deck of slides as object in JS
-const deck = document.getElementById('carousel__deck');
+//Initialise carousel-deck of slides as object in JS - x2 (landscape and portrait)
+const deck = document.getElementById('carousel__deck-landscape');
+const deckP = document.getElementById('carousel__deck-portrait');
+console.dir(deckP);
 
-//Initialise an array(object) of all slides from the deck
+//Initialise an array(object) of all slides from the deck - x2 (landscape and portrait)
 const slidesArr = Array.from(deck.children);
+const slidesArrP = Array.from(deckP.children);
+console.dir(slidesArrP);
 
 //Initialise current slide as object in JS
 let currentSlide = deck.querySelector('.current-slide');
+let currentSlideP = deckP.querySelector('.current-slide');
 
 //Initialise navigation dots div as object in JS
 const navDots = document.getElementById('dots');
@@ -44,64 +49,83 @@ const dotsArr = Array.from(navDots.children);
 let currentDot = navDots.querySelector('.current-dot');
 
 //Initialise individual arrows as objects in JS
-const prevArrow = document.querySelector(".carousel__arrows--prev");
-const nextArrow = document.querySelector(".carousel__arrows--next");
+const prevArrow = document.getElementById("carouselPrevArrow");
+const nextArrow = document.getElementById("carouselNextArrow");
 
 //Initialise carousel control button as object in JS
 const controlButton = document.getElementById('play-pause');
 
-//Declare a variable for the slide being targeted, to move to
+//Declare a variable for the slide being targeted, to move to - x2 (landscape and portrait)
 let targetSlide;
+let targetSlideP;
 
-//Declare variable for the distance to targetSide (or currentSlide on resize)
+//Declare variable for the distance to targetSide (or currentSlide on resize) - x2 (landscape and portrait)
 let dist;
+let distP;
 
 //Declare a variable ready for set interval method on 'play' button pushed
 let slideInterval;
 
-//Get width of individual slides and initialise as a variable (number)
+//Get width of individual slides and initialise as a variable (number) - x2 (landscape and portrait)
 let slideWidth = slidesArr[0].getBoundingClientRect().width;
+let slideWidthP = slidesArrP[0].getBoundingClientRect().width;
 
 //Initilise a variable for pop up span element for slide 3
 const pop = document.getElementById('popup');
 
 //Iniitalise a variable for slide 3 img element itself
-const slide3 = document.getElementById('slide3');
+const slide3 = document.getElementById('slideP3');
+const slideP3 = document.getElementById('slideP3');
 
 
 
-//Spread out the deck of slides in a line
+//Spread out the deck of slides in a line - x2 (landscape and portrait)
 //(i.e. instead of on top of each other now position = absolute)
 const positionSlide = (slide, index) => {
     slide.style.left = slideWidth * index + "px";
 }
 slidesArr.forEach(positionSlide);
+const positionSlideP = (slide, index) => {
+  slide.style.left = slideWidthP * index + "px";
+}
+slidesArrP.forEach(positionSlideP);
 
-//When window resizes, resize distance between target slides to resolve bug
+//When window resizes, resize distance between target slides to resolve bug - x2 (landscape and portrait)
 window.onresize = () => {
   //updated previous version (that used location reload)..
   //..by recalculating dist to current slide and transitioning with no delay
   //better than location reload, which refreshed to first slide each time
   slideWidth = slidesArr[0].getBoundingClientRect().width;
+  slideWidthP = slidesArrP[0].getBoundingClientRect().width;
   slidesArr.forEach(positionSlide);
+  slidesArrP.forEach(positionSlide);
   dist = currentSlide.style.left;
+  distP = currentSlideP.style.left;
   //remove timed transition used when shuffling with viewTargetSlide function
   deck.style.transitionDuration = '0ms';
+  deckP.style.transitionDuration = '0ms';
   //shuffle to target slide...
   deck.style.transform = 'translateX(-' + dist + ')';
+  deckP.style.transform = 'translateX(-' + distP + ')';
 }
 
-//Function to move target slide into the carousel viewer
-const viewTargetSlide = (targetSlide) => {
+//Function to move target slide into the carousel viewer - x2 (landscape and portrait)
+const viewTargetSlide = (targetSlide, targetSlideP) => {
   dist = targetSlide.style.left;
+  distP = targetSlideP.style.left;
   //confirm timed transition used when shuffling with viewTargetSlide function
   deck.style.transitionDuration = '500ms';
+  deckP.style.transitionDuration = '500ms';
   //shuffle to target slide...
   deck.style.transform = 'translateX(-' + dist + ')';
+  deckP.style.transform = 'translateX(-' + distP + ')';
   //update currentSlide
   currentSlide.classList.remove('current-slide');
+  currentSlideP.classList.remove('current-slide');
   targetSlide.classList.add('current-slide');
+  targetSlideP.classList.add('current-slide');
   currentSlide = deck.querySelector('.current-slide');
+  currentSlideP = deckP.querySelector('.current-slide');
   //update currentDot
   currentDot.classList.remove("current-dot");
   targetIndex = slidesArr.findIndex(slide => slide === currentSlide);
@@ -128,7 +152,7 @@ const viewTargetSlide = (targetSlide) => {
     pop.style.visibility = "visible";
   }
   else {
-      pop.style.visibility = "hidden"
+    pop.style.visibility = "hidden"
   }
 }
 
@@ -138,8 +162,9 @@ const playSlides = () => {
     //Change target slide if not at end of deck
     if(currentSlide !== slidesArr[slidesArr.length-1]) {
       targetSlide = currentSlide.nextElementSibling;
+      targetSlideP = currentSlideP.nextElementSibling;
       //Run function to view target slide
-      viewTargetSlide(targetSlide);
+      viewTargetSlide(targetSlide, targetSlideP);
     }
     //Or exit method and change button to prompt 'play'
     else {
@@ -158,10 +183,12 @@ const togglePop = () =>  {
   if(pop.innerHTML === "Click here to add 'class' to slide 3!"){
       pop.innerHTML = "Click for the 'prototype' slide 3!";
       slide3.src = "slides/Slide3b.png";
+      slideP3.src = "slides/SlideP3b.png";
   }
   else {
       pop.innerHTML = "Click here to add 'class' to slide 3!"
       slide3.src = "slides/Slide3a.png";
+      slideP3.src = "slides/SlideP3a.png";
   }
 }
 
@@ -172,13 +199,16 @@ controlButton.addEventListener('click', () => {
   if(!controlButton.classList.contains("pause") && currentSlide !== slidesArr[slidesArr.length-1]){
     controlButton.innerHTML = "Pause";
     targetSlide = currentSlide.nextElementSibling;
+    targetSlideP = currentSlideP.nextElementSibling;
     playSlides();
   }
   else if (!controlButton.classList.contains("pause") && currentSlide == slidesArr[slidesArr.length-1]) {
     controlButton.innerHTML = "Pause";
     targetSlide = slidesArr[0];
-    viewTargetSlide(targetSlide);
+    targetSlideP = slidesArrP[0];
+    viewTargetSlide(targetSlide, targetSlideP);
     targetSlide = slidesArr[1];
+    targetSlideP = slidesArrP[1];
     playSlides();
   }
   else {
@@ -190,15 +220,17 @@ controlButton.addEventListener('click', () => {
 //Identify target slide as next in the deck when the right arrow is clicked
 nextArrow.addEventListener('click', () => {
   targetSlide = currentSlide.nextElementSibling;
+  targetSlideP = currentSlideP.nextElementSibling;
   //Run function to view target slide
-  viewTargetSlide(targetSlide);
+  viewTargetSlide(targetSlide, targetSlideP);
 })
 
 //Identify target slide as previous in the deck when the right arrow is clicked
 prevArrow.addEventListener('click', () => {
   targetSlide = currentSlide.previousElementSibling;
+  targetSlideP = currentSlideP.previousElementSibling;
   //Run function to view target slide
-  viewTargetSlide(targetSlide);
+  viewTargetSlide(targetSlide, targetSlideP);
 })
 
 //Identify target slide as previous/next in the deck when the left/right keys are pressed
@@ -209,6 +241,7 @@ document.addEventListener('keydown', (e) => {
     if(e.keyCode === 37) {
       if(currentSlide !== slidesArr[0]) {
         targetSlide = currentSlide.previousElementSibling;
+        targetSlideP = currentSlideP.previousElementSibling;
         //Run function to view target slide
         viewTargetSlide(targetSlide);
       }
@@ -216,6 +249,7 @@ document.addEventListener('keydown', (e) => {
     else if(e.keyCode === 39){
       if(currentSlide !== slidesArr[slidesArr.length-1]) {
         targetSlide = currentSlide.nextElementSibling;
+        targetSlideP = currentSlideP.nextElementSibling;
         //Run function to view target slide
         viewTargetSlide(targetSlide);
       }
@@ -232,6 +266,7 @@ navDots.addEventListener('click', (e) => {
   else {
     targetIndex = dotsArr.findIndex(dot => dot === clickedDot);
     targetSlide = slidesArr[targetIndex];
+    targetSlideP = slidesArrP[targetIndex];
     viewTargetSlide(targetSlide);
   }
 })
