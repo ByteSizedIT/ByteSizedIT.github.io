7082b0721fcd0c9462b4441df703b6f052b113d0
ByteSized <60395899+ByteSizedIT@users.noreply.github.com>
2020-12-30 14:25:50 +0000

    Amended coding glossary, incl presentation of results for RWD; Amended HTML structure for RWD nav bar

--- whyFAC.js
+++ whyFAC.js
@@ -2,10 +2,10 @@
 
 
 // Initialise js variables for search box and suggestion elements
-const searchBox2 = document.querySelector('#text2');
-const empty2 = document.getElementById('empty2');
+const glossSearchText = document.querySelector('#glossSearchText');
+const emptyGloss = document.querySelector('#emptyGlossSearch');
 
-const suggestions = document.querySelector('.suggestions');                                     
+const suggestions = document.querySelector('#glossSuggestions');                                     
 
 // Initialise variable for endpoint location of reference json file stored as a gist
 const endpoint = 'https://gist.githubusercontent.com/ByteSizedIT/1c8561bb56e71e56a93d77a9a40741d9/raw/264477a4f1c159bbc0bc01db39b376db2a16168a/FACglossary.json';
@@ -16,62 +16,57 @@ const entries = [];
 
 // Use fetch API method, passing in the endpoint of the json reference file
 fetch(endpoint)
-    //convert/identify raw data returned in to a json format to make it readable
-    .then(response => response.json())
-    //spread each individual item from the json file on to the end of the entries array
-    .then(data => entries.push(...data));
+  //convert/identify raw data returned in to a json format to make it readable
+  .then(response => response.json())
+  //spread each individual item from the json file on to the end of the entries array
+  .then(data => entries.push(...data));
 
 
 // Create a function to filter entries returned by fetch API
-function findMatches(searchText, entries) {
-  return entries.filter(entry => {
-      const searchReg = new RegExp(searchText, 'gi');
-      return entry.term.match(searchReg) || entry.definition.match(searchReg);
-  })
+function findMatches(text, entries) {
+  const reg = new RegExp(text, 'gi');
+  return entries.filter(entry => entry.term.match(reg) || entry.definition.match(reg))
 }
 
 // Create a function to display found matches
-function displayMatches(e) {
-  //console.log(e);
-  //console.log(this);
-  //console.dir(this);
+function displayMatches() {
   const matches = findMatches(this.value, entries);
   const searchReg = new RegExp(this.value, 'gi');
   const html = matches.map(entry => {
-      const matchedTerm = entry.term.replace(searchReg, `<span class='highlight1'>${this.value}</span>`);
-      const matchedDef = entry.definition.replace(searchReg, `<span class='highlight2'>${this.value}</span>`);
-      const matchedLinks = entry.link.split(" ").map(link => {return `</br><a href="${link}" target="_blank">${link}</a>`}).join("");
-      return `
-      <li>
-          <!--<span><b>${entry.term}:</b> ${entry.definition}</span>-->
-          <span><b>${matchedTerm}:</b> ${matchedDef}</span></br>
-          <span><b>links:</b> ${matchedLinks}</span>
-      </li>
-      `;
+    const matchedTerm = entry.term.toUpperCase().replace(searchReg, `<span class='highlight1'>${this.value.toUpperCase()}</span>`);
+    const matchedDef = entry.definition.replace(searchReg, `<span class='highlight2'>${this.value}</span>`);
+    const matchedLinks = entry.link.split(" ").map(link => {return `</br><a href="${link}" target="_blank">${link}</a>`}).join("");
+    return `
+    <li>
+      <!--<span><b>${entry.term}:</b> ${entry.definition}</span>-->
+      <span><b>${matchedTerm}:</b> ${matchedDef}</span></br>
+      <span><b>links:</b> ${matchedLinks}</span>
+    </li>
+    `;
   }).join("");
   suggestions.innerHTML = html;
 }
   
 //Make the 'empty' button (X) appear in Search's text box (when text is entered)
-function toggleEmpty2() {
+function toggleEmptyGloss() {
   console.log(this.value);
   if(this.value == "") {
-      empty2.style.visibility = "hidden";
-      suggestions.innerHTML = "";
+    emptyGloss.style.visibility = "hidden";
+    suggestions.innerHTML = "";
   }
   else {
-      empty2.style.visibility = "visible";
+    emptyGloss.style.visibility = "visible";
   }
 }
 
 // Listener when typing new search term, to (find &) display matches
-searchBox2.addEventListener('keyup', displayMatches);
+glossSearchText.addEventListener('keyup', displayMatches);
 
 // Listener when typing/deleting search term, to show 'X'(empty) button or hide 'X' button and remove suggestions
-searchBox2.addEventListener("keyup", toggleEmpty2);
+glossSearchText.addEventListener("keyup", toggleEmptyGloss);
 
-empty2.addEventListener("click", () => {
-  empty2.style.visibility = "hidden";
-  searchBox2.value = "";
+emptyGloss.addEventListener("click", () => {
+  emptyGloss.style.visibility = "hidden";
+  glossSearchText.value = "";
   suggestions.innerHTML = "";
 });
